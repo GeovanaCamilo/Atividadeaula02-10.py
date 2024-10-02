d599b20d64d93ab8c30cb84fc0fdb7d14b614d9b
GeovanaCamilo <68429383+GeovanaCamilo@users.noreply.github.com>
2024-10-02 20:57:42 -0300

    Add files via upload

---
+++ Exercicio1 Aula 02-10.py
@@ -0,0 +1,19 @@
+
+N1 = 6
+N2 = 9
+primeiro_Nome = "Igor"
+sobrenome = "Professor"
+
+print(primeiro_Nome+ " "+sobrenome)
+
+soma = N1+N2
+
+print ("soma de ",N1 ,"é", N2 ,"é igual a:" , soma)
+
+numero1 = 9
+numero2 = 10
+numero3 = 6
+
+soma = (numero1 + numero2 + numero3) /3
+
+print (("a media é: "), soma)
